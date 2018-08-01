215d16c7e702652641f8ea61250d11ab1298d266
aminnurr <bystander567@outlook.com>
2018-08-01 03:30:30 +0100

    3

--- lib/tasks/sort_links.rb
+++ lib/tasks/sort_links.rb
@@ -3,27 +3,40 @@ require 'json'
 require 'nokogiri'
 
 
-#file = File.read "newdata.json"
+file = File.read "newdata.json"
 
-#data = JSON.parse(file)
- #data.each do |title|
- #	 url = title["url"]
- 	doc = Nokogiri::HTML(open('http://wiki.d-addicts.com/33_Gu_Shi_Guan' ))
- 	#puts doc 
+data = JSON.parse(file)
+ data.each do |title|
+ 	 url = title["url"]
+ 	#doc = Nokogiri::HTML(open('http://wiki.d-addicts.com/33_Gu_Shi_Guan' ))
+ 	doc =Nokogiri::HTML(open(url))
+ 	#  puts @english_title =doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]').text
+ 	  
+ 
 
- # puts doc.css('title').text
+ 
 
  
 if doc.xpath('//*[@id="Synopsis"]')
- #if  doc.css('title').text.include? "Profile"	
- 	puts  "this is a movie" 
+ 
+ 	#puts  "this is a movie"
+ 	puts  @english_title =doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]').text
+ 	json = JSON.pretty_generate(@english_title)
+ 	 File.open("movie_title.json","w") do |f|
+  f.write(json)
+end
+ 	# save this to a json file
 
-elsif     doc.xpath('//*[@id="Profile"]') 
-	       puts "this is a person "
- #     puts "this is a movie" + title
+       elsif doc.xpath('//*[@id="Profile"]') 
+	    # puts "this is a person "
+	     puts @name = doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]/text()').text
+	     json = JSON.pretty_generate(@people) 
+ 	     File.open("name.json","w") do |f|
+  f.write(json)
+	    end       
 
-      else 
-           puts nil
+	else 
+           puts "yeah"
 
            
           end
@@ -31,4 +44,4 @@ elsif     doc.xpath('//*[@id="Profile"]')
     # put it to sleep for 1 to 9 seconds   
 	
 
- #end 
+ end 
