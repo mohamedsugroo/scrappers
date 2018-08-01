dda0843c62676d2cf1f2f1f76967643da28e31ce
aminnurr <bystander567@outlook.com>
2018-08-01 15:47:22 +0100

    movie_scrappers change

--- lib/tasks/people_sort_links.rb
+++ lib/tasks/people_sort_links.rb
@@ -1,45 +1,28 @@
-#require 'open-uri'
-#require 'json'
-#require 'nokogiri'
+require 'open-uri'
+require 'json'
+require 'nokogiri'
 
 
-#file = File.read "newdata.json"
 
-#data = JSON.parse(file)    # breaks , only works for movies and actors urls , not for others
- #data.each do |title|
- 	# url = title["url"]
- 	#doc = Nokogiri::HTML(open('http://wiki.d-addicts.com/33_Gu_Shi_Guan' ))
- #	doc =Nokogiri::HTML(open(url))
- 	#  puts @english_title =doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]').text
- 	 
+
+data = JSON.parse(file)   
+ data.each do |title|
+ 	 url = title["url"]
+ 	
+ 	doc =Nokogiri::HTML(open(url))
  
+ 	  if doc.xpath('//*[@id="Profile"]') 
+	     puts "this is a person "
+         @name = doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]/text()').text
+     json = JSON.pretty_generate(@people) 
+      File.open("name.json","w") do |f|
+  f.write(json)
+	  end       
 
- 
-
- 
-#if doc.xpath('//*[@id="Synopsis"]')
- 
- 	#puts  "this is a movie"
- #	puts  @english_title =doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]').text
- #	json = JSON.pretty_generate(@english_title)
- #	 File.open("movie_title.json","w") do |f|
-  #f.write(json)
-#end
- 	# save this to a json file
-
- #      elsif doc.xpath('//*[@id="Profile"]') 
-	    # puts "this is a person "
-#	     puts @name = doc.xpath('//*[@id="mw-content-text"]/ul[1]/li[2]/text()').text
-#	     json = JSON.pretty_generate(@people) 
- #	     File.open("name.json","w") do |f|
-  #f.write(json)
-	#    end       
-
-	#else 
-     #      puts nil 
+	
 
            
-      #    end
+          end
 
     # put it to sleep for 1 to 9 seconds   
 	
